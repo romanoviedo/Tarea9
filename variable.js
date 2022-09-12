efcaa3ce82c498efb7d0c46267c924f9c8e2124b
romanoviedo <113319638+romanoviedo@users.noreply.github.com>
2022-09-11 23:25:25 -0300

    Tarea9
    Tarea9

---
+++ variable.js
@@ -0,0 +1,10 @@
+var pais="Italia";
+var continente="Europa";
+var antiguedad="1861";
+var pais_y_continente= pais +""+ continente;
+
+pais="Argentina";
+continente="Latinoamerica";
+
+console.log (pais, continente, antiguedad)
+alert (pais_y_continente)
